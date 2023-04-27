efa8f21cccb43ad63718e66d1fe35874f7ef3361
ShadowShahriar <shadowshahriar.dev@gmail.com>
2023-04-27 23:54:30 +0600

    Update quote message structure

--- send-quote.js
+++ send-quote.js
@@ -3,7 +3,13 @@ import { random, sendMessage, sendRandomMessage, getRandomQuote } from './utils.
 
 async function sendQuote() {
 	const quote = await getRandomQuote()
-	const messages = [`হঠাৎ করে ${quote.author}-র এই উক্তিটা মনে পড়ে গেল -`, `${quote.author}-র এই কথাটা আগে শুনেছ?`]
+	const messages = [
+		`হঠাৎ করে ${quote.author}-র এই উক্তিটা মনে পড়ে গেল -`,
+		`${quote.author} once said,`,
+		`${quote.author}-র এই কথাটা আগে শুনেছ?`,
+		`Here is a quote by ${quote.author}`,
+		`Did you know that ${quote.author} said,`,
+	]
 	await sendMessage(random(greet))
 	await sendMessage(random(messages))
 	await sendMessage(`"${quote.content}"`)
