6871d59aaf1c617ecbf98d00b45331bbc21f7592
ShadowShahriar <shadowshahriar.dev@gmail.com>
2023-04-27 23:53:47 +0600

    Update messages

--- config.js
+++ config.js
@@ -1,35 +1,79 @@
-const greet = ['ভাই', 'দোস্ত', 'বন্ধু', 'আছো?', 'ভাই আছো?']
+const greet = ['ভাই', 'দোস্ত', 'শাহ্‌রিয়ার', 'বন্ধু', 'Heyy~', 'আছো?', 'Shahriar', 'Shadow', 'ভাই আছো?']
 
 const reminderGIFs = [
-	'ready',
-	'write',
-	'writing',
-	'cheering',
-	'reading',
+	'excited-anime-boy',
+	'anime-boy-thumbs-up',
+	'anime-boy-happy',
+	'anime-boy-smile',
 	'all-the-best',
-	'thumbsup',
 	'thumbs-up',
-	'good-luck',
-	'best-of-luck',
-	'excited',
+	'anime-boy-writing',
+	'anime-boy-reading',
+	'anime-reading',
+	'anime-boy-cheering',
+	'anime-boy-clapping',
+	'anime-boy-yay',
 ]
 
 const reminders = [
-	['Aso?', 'Golpo lekhar kotha mone ache tw?'],
-	['Ja golpo lekh. Abar vule jabi', 'Parle at least plot line gulo khatay tuke rakh'],
-	['Complete the stories dear!'],
-	['Vai', 'aso?', 'Golpo lekho'],
-	['All the best for writing your brand new story. I hope people would love to read it!'],
+	['শাহ্‌রিয়ার আছো নাকি?'],
+	['আছো?', 'গল্প লেখার কথা মনে আছে তো? 👀'],
+	['যা, গল্প লেখ! আবার ভুলে যাবি, 😒', 'পারলে at least প্লট লাইনগুলো খাতায় টুকে রাখ!'],
+	['Complete the stories dear! 💛'],
+	['শাহ্‌রিয়ার', 'আছো?', 'গল্প লেখো'],
+	['Heyy~', 'Are you writing a new story?'],
+	['কি ভাই! গল্প লিখছো নাকি?'],
+	['Write that story, dude! 👊🏼'],
+	['ভাই', 'আছো?', 'গল্প লেখো'],
+	['All the best for writing your new story. I hope people would love to read it! 😊'],
 	["Don't forget to write the story and showing me! 😁"],
 ]
 
+const chores = [
+	['Today is a great day to check the e-mails, right?'],
+	['It feels like a good day to read some e-mails! 😄'],
+	['আজকে ঘরটা একটু গুছিয়ে ফেললে কেমন হয়? 😁'],
+	['আজকে পড়ার টেবিলটা গুছিয়ে ফেলে আম্মুকে surprise করে দিও! ✨'],
+	['ল্যাপটপে ধূল জমেছে, পারলে একটু পরিষ্কার করে ফেলো'],
+	['সময় পেলে ঘরটা একটু গুছিয়ে ফেল, ঠিক আছে? 😊'],
+]
+
+const birthdayWishes = [
+	['Happy Birthday, bro! 🤗'],
+	['Happy Birthday, dude!'],
+	['Happy Birthday, Shah! 🥰'],
+	['Heyyyyyy~ Happy Birthday! 🎉🎉🎉🎉'],
+	['Happiest birthday brother! ✨'],
+	['Happy Birthday, Shahriar! 😄', '🍰'],
+	['🍰', 'Here, have some cake'],
+	['শুভ জন্মদিন বন্ধু! 🎉'],
+	['শুভ জন্মদিন 😄'],
+	['Yo. Happy Birthday, Shahriar 🍰'],
+	['Happy Birthday, friend 🌻'],
+	['নে কেক খা!', '🎂'],
+	['🍫', 'চকলেট খাবি? 😁'],
+]
+
+const cheerfulComments = [
+	["It's okay dear. You are doing great!"],
+	['You are doing great! ✨'],
+	['You are great! 🌟'],
+	['You are trying your best'],
+	["You're on fire 🔥"],
+	['Your work is awesome 🙌'],
+	['You are doing pretty well and I know you can do even better'],
+	['You are getting better every day ✌'],
+	["That's some good progress there! 😎👏"],
+]
+
 const videoComments = [
-	['vai eita joss'],
+	['ভাই এইটা জোস! 😆'],
 	['joss na?'],
-	['joss'],
-	['eta dekh'],
-	['eta dekho, valo lagbe'],
-	['eta deikho', 'valo lagbe'],
+	['joss 🔥'],
+	['এটা দ্যাখ! 👀'],
+	['এটা দেখো, ভালো লাগবে'],
+	['এইটা দেইখো', 'ভালো লাগবে 😊'],
+	['ভাই', 'এইটা দেখেন', 'মন ভালো হইয়া যাইব 😁'],
 ]
 
 const quoteComments = [
@@ -38,13 +82,16 @@ const quoteComments = [
 	['ঠিক না?'],
 	['কথাটা কিন্তু জোস!', 'তাই না?'],
 	['তাই না রে?'],
+	['তাই না, শাহ্‌রিয়ার?'],
+	['Right, no?'],
 	['সেই না?'],
 	['সেইরকম একটা কথা!'],
 	['mind blown!'],
-	['ভাইরে ভাই সেই একটা কথা!'],
+	['ভাইরে ভাই সেইই একটা কথা! 🤩'],
+	["Isn't it?"],
 ]
 
-const photoCategories = ['cat', 'cats', 'birds', 'flower', 'flowers']
+const photoCategories = ['cat', 'cats', 'birds', 'flower', 'flowers', 'sunset']
 const photoComments = [
 	['ভাইরে ভাই'],
 	['👀'],
@@ -58,4 +105,15 @@ const photoComments = [
 	['😁'],
 ]
 
-export { greet, reminders, reminderGIFs, quoteComments, photoComments, photoCategories, videoComments }
+export {
+	greet,
+	reminders,
+	reminderGIFs,
+	quoteComments,
+	photoComments,
+	photoCategories,
+	videoComments,
+	chores,
+	birthdayWishes,
+	cheerfulComments,
+}
