c5d08b65e2839b19357e8849825000eeb233d847
ShadowShahriar <shadowshahriar.dev@gmail.com>
2023-04-28 17:09:54 +0600

    Add birthdays of known people i1

---
+++ birthdays.js
@@ -0,0 +1,83 @@
+const birthdays = {
+	// === January ===
+	'03-01': { person: 'মাহাতাব', role: '' },
+	'04-01': { person: 'সুজানা', role: 'পিচ্চি' },
+	'27-01': { person: 'তালহা', role: '' },
+	'31-01': { person: 'অনিশা', role: '' },
+
+	// === February ===
+	'02-02': { person: 'জহির', role: '' },
+	'17-02': { person: 'রিতিকা', role: 'দি' },
+	'20-02': { person: ['চারু', 'গম্ভীর'], role: ['দি', 'ভাই'] },
+	'21-02': { person: 'তুহিন', role: '' },
+
+	// === March ===
+	'09-03': { person: 'Portious', role: 'ভাইয়া' },
+	'17-03': { person: 'পাঠান', role: 'সাহেব' },
+	'18-03': { person: ['ফারিয়া', 'অনন্ত'], role: ['পিচ্চি', 'ভাই'] },
+	'24-03': { person: 'তানসিফ', role: '' },
+	'26-03': { person: ['ইসমাইল', 'অনিন্দ্য'], role: ['', 'দা'] },
+
+	// === April ===
+	'13-04': { person: 'আফসারা', role: '' },
+	'20-04': { person: 'রূপা', role: 'খালামনি' },
+	'27-04': { person: 'তুতুল', role: 'ভাইয়া' },
+
+	// === May ===
+	'08-05': { person: 'তারাক্কি', role: 'সাহেব' },
+	'11-05': { person: 'স্বর্ণালি', role: '' },
+	'28-05': { person: 'আম্মু', role: '' },
+
+	// === June ===
+	'03-06': { person: ['বাবা', 'সারাহ'], role: ['', 'আপু'] },
+	'07-06': { person: 'বড় মামা', role: '' },
+	'11-06': { person: 'অমিত', role: 'দা' },
+	'20-06': { person: 'Itay', role: 'স্যার' },
+	'24-06': { person: 'ধ্রুব', role: '' },
+
+	// === July ===
+	'06-07': { person: 'ফয়সাল', role: '' },
+	'20-07': { person: ['জারা', 'জায়ান'], role: '' },
+
+	// === August ===
+	'06-08': { person: 'পৃথা', role: '' },
+	'08-08': { person: 'লুবু', role: 'পিচ্চি' },
+	'15-08': { person: 'সাব্বির', role: '' },
+	'24-08': { person: 'নুসরাত', role: 'আপু' },
+
+	// === September ===
+	'02-09': { person: 'রিপা', role: 'খালামনি' },
+	'08-09': { person: ['সিনথিয়া', 'সৈকত'], role: ['', 'দা'] },
+	'09-09': { person: 'Khanh', role: '' },
+	'12-09': { person: 'তারিকুল', role: '' },
+	'14-09': { person: 'অর্পণ', role: '' },
+	'15-09': { person: 'মাইশা', role: '' },
+	'17-09': { person: 'হাসান', role: 'ভাইয়া' },
+	'20-09': { person: 'জোবায়ের', role: '' },
+
+	// === October ===
+	'09-10': { person: 'কৃষ', role: 'ভাইয়া' },
+	'20-10': { person: 'ফাইয়ায', role: 'পিচ্চি' },
+	'26-10': { person: 'অদিতি', role: 'আপু' },
+	'27-10': { person: 'Sunrit', role: '' },
+	'30-10': { person: ['অনিক', 'খাইরুল'], role: ['ভাই', ''] },
+
+	// === November ===
+	'06-11': { person: 'রোজা', role: '' },
+	'14-11': { person: 'রেজা', role: 'মামা' },
+	'19-11': { person: 'সিয়াম', role: 'স্কুলের', prefix: true },
+
+	// === December ===
+	'05-12': { person: ['ছোট মামা', 'সৃষ্টি'], role: ['', ''] },
+	'09-12': { person: ['রাইসা', 'প্রীত'], role: ['আপু', 'ভাইয়া'] },
+	'14-12': { person: 'অ্যানি', role: 'মিস' },
+}
+
+export default birthdays
+
+// examples:
+// '27-04': { person: 'অনিক', role: 'vai' },
+// '26-04': { person: ['Mahatab', 'Azmain'], role: 'vai' },
+// '30-04': { person: ['Nusrat', 'Risha'], role: 'apu', prefix: true },
+// '01-05': { person: ['Anita', 'Towfiq'], role: ['', 'boro vai'], prefix: [true] },
+// '01-05': { person: ['Anita', 'Towfiq'], role: ['didi', 'boro vai'], prefix: [false, true] },
