d60d8ae4087dd07b7083475e84881b831d323f93
ShadowShahriar <shadowshahriar.dev@gmail.com>
2023-04-29 22:20:14 +0600

    Add reminder for drawing

--- send-reminder.js
+++ send-reminder.js
@@ -1,10 +1,19 @@
-import { reminders, reminderGIFs } from './config.js'
-import { sendRandomMessage, getGIFsFromTenor, random, sendGIF } from './utils.js'
+import { reminders, reminderGIFs, sketchReminders, sketchReminderGIFs, greet } from './config.js'
+import { sendRandomMessage, getGIFsFromTenor, random, sendGIF, sendMessage } from './utils.js'
 
 async function sendReminder() {
-	const GIFs = await getGIFsFromTenor(random(reminderGIFs))
-	await sendRandomMessage(reminders)
-	await sendGIF(random(GIFs))
+	const date = new Date().getDate()
+
+	if (date % 2 === 0) {
+		const GIFs = await getGIFsFromTenor(random(reminderGIFs))
+		await sendRandomMessage(reminders)
+		await sendGIF(random(GIFs))
+	} else {
+		const GIFs = await getGIFsFromTenor(random(sketchReminderGIFs))
+		await sendMessage(random(greet))
+		await sendRandomMessage(sketchReminders)
+		await sendGIF(random(GIFs))
+	}
 }
 
 sendReminder()
