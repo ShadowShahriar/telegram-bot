464e675691b3fac861c2152d2ff6416452682d2a
ShadowShahriar <shadowshahriar.dev@gmail.com>
2023-08-07 01:29:36 +0600

    Add friendship songs

--- src/config.js
+++ src/config.js
@@ -99,4 +99,24 @@ const stickers = {
 	]
 }
 
-export { user, query, script, stickers }
+const songs = {
+	friendship: [
+		'https://youtu.be/-lgDyoWGCa4',
+		'https://youtu.be/WOi1HgpxEmM',
+		'https://youtu.be/yZ-RyC1b1sw',
+		'https://youtu.be/Z8N3kpRSk0M',
+		'https://youtu.be/O1HOn-KckDc',
+		'https://youtu.be/kvxMdwiZOv8',
+		'https://youtu.be/sZdmkif8CSY',
+		'https://youtu.be/EatzcaVJRMs',
+		'https://youtu.be/tmLBITpyxPc',
+		'https://youtu.be/oRjHv3jK6Uc',
+		'https://youtu.be/-z_UvHknyrc',
+		'https://youtu.be/xC1cj9zhh6k',
+		'https://youtu.be/BTRPBiE_1lA',
+		'https://youtu.be/xLUCo7lhnxE',
+		'https://youtu.be/LpGhUuxRgDs'
+	]
+}
+
+export { user, query, script, stickers, songs }
