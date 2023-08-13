944100a653b7a5c7329b1dde8a2cea74b75f95b1
ShadowShahriar <shadowshahriar.dev@gmail.com>
2023-08-14 02:09:11 +0600

    Fix date issues in src/w-moment.js

--- src/w-moment.js
+++ src/w-moment.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import { script } from './config.js'
-import { getWeekdays, random } from './utils.js'
+import { getWeekdays, months, random } from './utils.js'
 import { sendPhoto, sendMessage } from './api/telegram.js'
 import { readRecord, writeRecord, getStorageURL } from './api/firebase.js'
 import { aboutMoment } from './data/messages.js'
@@ -12,14 +12,24 @@ function isToday(d) {
 	return D.getDate() === d.getDate() && D.getMonth() === d.getMonth()
 }
 
+function pretty(d) {
+	return `${d.getDate()} ${months.short[d.getMonth()]}`
+}
+
 async function main() {
 	console.time()
 
-	const secondSunday = getWeekdays('Sun')[1]
-	const thirdTuesday = getWeekdays('Tue')[2]
+	const secondDay = getWeekdays('Sun')[1]
+	const thirdDay = getWeekdays('Mon')[3]
+	console.log('Today:'.padStart(14), pretty(new Date()))
+	console.log('Second Sunday:', pretty(secondDay))
+	console.log('Fourth Monday:', pretty(thirdDay))
+
 	if (process.env.TESTING_ON_PUSH) {
 	} else {
-		if (!isToday(secondSunday) || !isToday(thirdTuesday)) {
+		if (isToday(secondDay) || isToday(thirdDay)) {
+			console.log('This is the moment')
+		} else {
 			console.log('Aborted')
 			console.timeEnd()
 			return
@@ -41,7 +51,7 @@ async function main() {
 	}
 	await writeRecord(record, nextRecord)
 
-	const file = `${choice}.${fileExt}`
+	const file = `${(choice + '').padStart(2, '0')}.${fileExt}`
 	await sendPhoto(getStorageURL(directory, file))
 	await sendMessage(random(aboutMoment))
 	console.timeEnd()
